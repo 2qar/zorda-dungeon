25db5f570734205e9a1626e748289354d2542cdb
Khujou <svilpathomas@gmail.com>
2020-06-16 14:08:48 -0700

    added color differences between floor and wall using the constructor

--- zorda-dungeon/Room.cs
+++ zorda-dungeon/Room.cs
@@ -16,14 +16,17 @@ namespace zorda_dungeon
 	{
 		Texture2D block;
 		RoomDirection roomDirection;
-		Color color;
+		Color floorColor;
+		Color wallColor;
 
 		// Room constructor: block texture, room direction, and color.
 		public Room(Texture2D block, RoomDirection roomDirection, Color color)
 		{
 			this.block = block;
 			this.roomDirection = roomDirection;
-			this.color = color;
+			this.floorColor = color;
+			this.wallColor = this.floorColor;
+			this.wallColor.A += 120;
 		}
 
 		public bool checkRoomDirection(RoomDirection roomDir)
@@ -40,7 +43,7 @@ namespace zorda_dungeon
 
 		public void Draw(SpriteBatch spriteBatch)
 		{
-			spriteBatch.Draw(block, new Rectangle(64, -20, 668, 520), Color.DarkGreen);
+			spriteBatch.Draw(block, new Rectangle(64, -20, 668, 520), floorColor);
 
 			for (int i = 0; i <= 6; i++)
 			{
@@ -57,16 +60,16 @@ namespace zorda_dungeon
 								continue;
 
 						}
-						spriteBatch.Draw(block, new Rectangle(110 + 64 * k, 15 + 64 * i, 64, 64), Color.Green);
+						spriteBatch.Draw(block, new Rectangle(110 + 64 * k, 15 + 64 * i, 64, 64), wallColor);
 					}
 				}
 				else
 				{
 					if (!(i == 3 && checkRoomDirection(RoomDirection.Left)))
-						spriteBatch.Draw(block, new Rectangle(110, 15 + 64 * i, 64, 64), Color.Green);
+						spriteBatch.Draw(block, new Rectangle(110, 15 + 64 * i, 64, 64), wallColor);
 
 					if (!(i == 3 && checkRoomDirection(RoomDirection.Right)))
-						spriteBatch.Draw(block, new Rectangle(622, 15 + 64 * i, 64, 64), Color.Green);
+						spriteBatch.Draw(block, new Rectangle(622, 15 + 64 * i, 64, 64), wallColor);
 
 				}
 			}
