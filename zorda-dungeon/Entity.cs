bec0728dec8d8659ad6678289b0434aa95bbd3c7
Khujou <svilpathomas@gmail.com>
2020-06-16 15:41:31 -0700

    made entity class for future use with character and enemies

---
+++ zorda-dungeon/Entity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace zorda_dungeon
+{
+
+	public class Entity
+	{
+		SpriteBatch spriteBatch;
+		Texture2D sprite;
+		Vector2 position;
+		float moveSpeed;
+		Rectangle sourceRectangle;
+
+		//Constructor: Sprite texture, move speed, 
+		public Entity(SpriteBatch spriteBatch, Texture2D sprite, float moveSpeed)
+		{
+			this.spriteBatch = spriteBatch;
+			this.sprite = sprite;
+			this.moveSpeed = moveSpeed;
+		}
+
+
+	}
+}
